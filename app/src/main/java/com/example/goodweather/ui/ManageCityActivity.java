0cc32a5ce01d31d5f2f6ace448a7c8e03440b948
fukai <fukai@xiaomi.com>
2025-01-10 14:45:32 +0800

    添加城市，通过推荐城市选择。

--- app/src/main/java/com/example/goodweather/ui/ManageCityActivity.java
+++ app/src/main/java/com/example/goodweather/ui/ManageCityActivity.java
@@ -3,26 +3,20 @@ package com.example.goodweather.ui;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
-import android.util.Log;
-import android.view.View;
-import android.view.WindowInsetsController;
 
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
-import com.google.gson.Gson;
 import com.example.goodweather.Constant;
 import com.example.goodweather.databinding.ActivityManageCityBinding;
 import com.example.goodweather.db.bean.MyCity;
-import com.example.goodweather.db.bean.Province;
 import com.example.goodweather.ui.adapter.MyCityAdapter;
-import com.example.goodweather.ui.adapter.OnClickItemCallback;
+import com.example.goodweather.utils.AddCityDialog;
 import com.example.goodweather.viewmodel.ManageCityViewModel;
 import com.example.library.base.NetworkActivity;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -37,6 +31,7 @@ public class ManageCityActivity extends NetworkActivity<ActivityManageCityBindin
     @Override
     protected void onCreate() {
         initView();
+
         viewModel = new ViewModelProvider(this).get(ManageCityViewModel.class);
         viewModel.getAllCityData();
     }
@@ -44,20 +39,34 @@ public class ManageCityActivity extends NetworkActivity<ActivityManageCityBindin
     private void initView() {
         backAndFinish(binding.toolbar);
         setStatusBar(true);
-        myCityAdapter.setOnClickItemCallback(position -> {
-            Intent intent = new Intent();
-            intent.putExtra(Constant.CITY_RESULT, myCityList.get(position).getCityName());
-            setResult(Activity.RESULT_OK, intent);
-            finish();
-        });
+        myCityAdapter.setOnClickItemCallback(position -> setPageResult(myCityList.get(position).getCityName()));
         binding.rvCity.setLayoutManager(new LinearLayoutManager(ManageCityActivity.this));
         binding.rvCity.setAdapter(myCityAdapter);
-        binding.btnAddCity.setOnClickListener(v -> showMsg("添加城市"));
+
+        binding.btnAddCity.setOnClickListener(v ->
+                AddCityDialog.show(ManageCityActivity.this, Arrays.asList(Constant.CITY_ARRAY), cityName -> {
+                    //保存到数据库中
+                    viewModel.addMyCityData(cityName);
+                    //设置页面返回数据
+                    setPageResult(cityName);
+                }));
+    }
+
+    /**
+     * 设置页面返回数据
+     * @param cityName 城市名
+     */
+    private void setPageResult(String cityName) {
+        Intent intent = new Intent();
+        intent.putExtra(Constant.CITY_RESULT, cityName);
+        setResult(Activity.RESULT_OK, intent);
+        finish();
     }
 
     @SuppressLint("NotifyDataSetChanged")
     @Override
     protected void onObserveData() {
+        //我的城市所有数据返回
         viewModel.listMutableLiveData.observe(this, myCities -> {
             if (myCities != null && myCities.size() > 0) {
                 myCityList.clear();
@@ -68,4 +77,5 @@ public class ManageCityActivity extends NetworkActivity<ActivityManageCityBindin
             }
         });
     }
+
 }
